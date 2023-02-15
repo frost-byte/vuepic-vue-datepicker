fbe2945a17ff0dab3bc92059cedd8ff72859d6ef
frost-byte <brian@frost-byte.net>
2023-02-15 15:28:00 -0500

    1 file modified

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -1,5 +1,4 @@
 <script setup>
-// week-picker
 import { ref, onMounted } from 'vue';
 import Datepicker from '@vuepic/vue-datepicker';
 import { DateExtent, extents } from '../interfaces/DateExtentInterface';
@@ -9,16 +8,22 @@ import '@vuepic/vue-datepicker/dist/main.css';
 // const extents: Map<string, string> = new Map([]);
 // const currentExtent: DateExtent = extents.get('day');
 
-const date = ref();
+const week = ref();
+const month = ref();
+
 onMounted(() => {
-  const startDate = new Date();
-  const endDate = new Date(new Date().setDate(startDate.getDate() + 7));
-  date.value = [startDate, endDate];
+  const startMonth = { month: 2, year: 2023 };
+  const endMonth = { month: 3, year: 2023 };
+  month.value = [startMonth, endMonth];
+  const startWeek = '02/05/2023';
+  const endWeek = '02/19/2023';
+  week.value = [startWeek, endWeek];
 });
 </script>
 
 <template>
   <div>
-    <Datepicker v-model="date" range />
+    <Datepicker v-model="month" range month-picker week-start="0" />
+    <Datepicker v-model="week" range week-picker week-start="0" />
   </div>
 </template>
